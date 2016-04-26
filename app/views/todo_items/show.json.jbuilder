b629576a0036f31c28d0b1646fb749f4f6807f8b
Deanna Melin <deannamelin@gmail.com>
2016-04-26 13:36:03 -0400

    initial rails commit

---
+++ app/views/todo_items/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @todo_item, :id, :description, :deadline, :priority, :complete, :created_at, :updated_at
---
+++ db/migrate/20160426155859_create_todo_items.rb
@@ -0,0 +1,12 @@
+class CreateTodoItems < ActiveRecord::Migration
+  def change
+    create_table :todo_items do |t|
+      t.string :description
+      t.datetime :deadline
+      t.integer :priority
+      t.boolean :complete
+
+      t.timestamps null: false
+    end
+  end
+end
